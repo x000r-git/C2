f42921f7f309bf23aed48dc688615cc82bdeb1bc
x000r-git <85619634+x000r-git@users.noreply.github.com>
2023-06-16 17:34:22 +0000

    Add test from local computer

--- test.cpp
+++ test.cpp
@@ -4,7 +4,7 @@
 std::string ParseMACAddress(const std::vector<uint8_t>& buffer, std::size_t offset) {
     std::string mac;
     static const char* digits = "0123456789ABCDEF";
-     if (offset + 6 >= buffer.size()) {
+     if (offset + 6 > buffer.size()) {
         throw std::string("MACAddress: lack buffer\n");
     }
 
@@ -25,8 +25,26 @@ bool badCommand(std::string &com){
     return false;
 }
 
-bool checkEq(char * a, char * b){
+bool checkEq(const char * a, const char * b){
     if (!strcmp(a, b))
         return true;
     return false;
+}
+
+TEST_CASE("testing checkEq function") {
+    CHECK(checkEq("clientss", "clients") == false);
+    CHECK(checkEq("clients", "clients") == true);
+    CHECK(checkEq("clies", "clients") == false);
+}
+
+TEST_CASE("checking sql statement parsing"){
+    std::string first = "SELECT * FROM biba;";
+    CHECK(badCommand(first) == true);
+    std::string command = "SELECT";
+    CHECK(badCommand(command) == false);
+}
+
+TEST_CASE("checking mac-address parsing"){
+    std::vector<uint8_t> check = {132, 156, 189, 222, 109, 155};
+    CHECK(ParseMACAddress(check, 0) == "84-9C-BD-DE-6D-9B");
 }
